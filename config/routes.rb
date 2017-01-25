920208856900bdfec0727fe8c482304e98cbc56d
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-01-25 15:12:04 -0800

    Fix route syntax for categories#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :categories do 
-    resources :recipes 
-  end 
+  resources :categories do
+    resources :recipes
+  end
 
   resources :ingredients
 
-  
-  get '/' => 'category#index'
+
+  get '/' => 'categories#index'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
